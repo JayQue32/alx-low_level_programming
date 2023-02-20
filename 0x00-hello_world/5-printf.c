4a968971ffc7b90ef121cbc7d6371779f1c4abac
root <ademolaademola232@gmail.com>
2023-02-20 12:31:00 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program to print with the use of printf function
+ * Return: 0 (SUCCESS)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+	return (0);
+}
