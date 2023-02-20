a54e00e67a23540ba3708927870f85c4f633b37c
root <ademolaademola232@gmail.com>
2023-02-20 12:18:59 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program printing a command line with the use of the puts function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle \n");
+		return (0);
+}
+
